3d010cf9e135b4c8d82b246b6f732a50dee4dba8
agusVal <titivalenti24@gmail.com>
2017-11-07 11:31:26 -0300

    primera parte

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var padre = document.getElementById('conte');
+var mensaje = document.getElementById('ingresar');
+document.getElementById('igresar').value = "";
+var dibujo = document.createElement('div');
+var parrafo = document.createElement('p');
+var nodo = document.createTextNode('mensaje');
+
+parrafo.appendChild('nodo');
+dibujo.appendChild('parrafo');
+padre.appendChild('dibujo');
+
+
+
+var boton = document.getElementById('btn');
+
+boton.addEventListener('click', funtion(){
+
+    
+
+});
+
+
