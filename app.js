ce46f0d01ec1248b31d8c154ded36e7fd34f1957
agusVal <titivalenti24@gmail.com>
2017-11-12 21:03:21 -0300

    agregando cosas

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+//parecido al menu de hamburguesa-vlver a ver
 
-var addList = document.getElementById("cartel");
+var AgregarLista = document.getElementById("cartel");
 
 
-addList.addEventListener("click", function(){
+AgregarLista.addEventListener("click", function(){
 
-	document.getElementById("pestaña").classList.add("after");
-	document.getElementById("botones").classList.remove("hidden");
+	document.getElementById("pestaña").classList.add("agregar");
+	document.getElementById("botones").classList.remove("remover");
 });
 
-//aplicar siguiente funcion, para guardar datos en la listas
+//aplicar siguiente funcion, para cerrar la lista.
+//igual a lo anterior, mismos pasos .
+
+var cerrarLista = document.getElementById("cerrar");
+
+   
+cerrarLista.addEventListener("click", function(){
+
+	document.getElementById("pestaña").classList.remove("agregar");
+	document.getElementById("botones").classList.add("remover");
+});
+
+//funcion para abrir mueva lita. Preguntar como crear listar y que poision deben tomar.
